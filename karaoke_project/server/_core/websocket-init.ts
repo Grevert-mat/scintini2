f6854c5bf3ea14b34de0e0cd5ff97506862fc74e
Grevert-mat <ahsscintini@gmail.com>
2026-04-06 12:58:14 -0300

    Delete karaoke_project directory

--- karaoke_project/server/_core/websocket-init.ts
+++
@@ -1,8 +0,0 @@
-import { Server as HTTPServer } from "http";
-import { initializeWebSocket } from "../websocket";
-
-export function setupWebSocket(httpServer: HTTPServer) {
-  const io = initializeWebSocket(httpServer);
-  console.log("[WebSocket] Socket.IO initialized");
-  return io;
-}
